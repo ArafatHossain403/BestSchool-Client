edc3d8f76b7e7a174e7f9632d9eea4556e805643
Arafat <arafatnubt@gmail.com>
2023-07-24 21:56:41 +0600

    set up

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQFMbUQ16JJyh8AmjutPAaT1ikXlPwQ5U",
+  authDomain: "bestschool-client.firebaseapp.com",
+  projectId: "bestschool-client",
+  storageBucket: "bestschool-client.appspot.com",
+  messagingSenderId: "133026911423",
+  appId: "1:133026911423:web:03f15024d6bc76b0e6a20a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
